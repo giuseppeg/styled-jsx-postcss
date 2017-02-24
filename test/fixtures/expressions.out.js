29d033d76394d5a34619ddd6608802b7f09a1ff2
Giuseppe Gurgone <giuseppeg@users.noreply.github.com>
2017-02-24 12:06:35 +0100

    fixture

--- test/fixtures/expressions.out.js
+++ test/fixtures/expressions.out.js
@@ -1,8 +1,7 @@
-const color = 'red'
+import _JSXStyle from 'styled-jsx/style';
+const color = 'red';
 
-export default () => (
-  <div>
-    <p>test</p>
-    <style jsx>{`p { img { display: block} color: ${color} }`}</style>
-  </div>
-)
+export default (() => <div data-jsx={1108639175}>
+    <p data-jsx={1108639175}>test</p>
+    <_JSXStyle styleId={1108639175} css={`p[data-jsx="1108639175"] {color: ${color} } p[data-jsx="1108639175"] img[data-jsx="1108639175"] {display: block}`} />
+  </div>);
